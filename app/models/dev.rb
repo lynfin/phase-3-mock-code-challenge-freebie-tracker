9449cef4a3cb4bfc5fdd114b1a2a6129038da9d1
Lyn <lynfinman@gmail.com>
2022-07-07 11:59:36 -0400

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.index { |freebie| freebie.item_name == item_name } ? true : false
+  end
+
+  def give_away(dev, freebie)
+    freebie.update(dev: dev) if freebie.dev.id == id
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
-puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+# puts 'Creating companies...'
+# Company.create(name: 'Google', founding_year: 1998)
+# Company.create(name: 'Facebook', founding_year: 2004)
+# Company.create(name: 'Dunder Mifflin', founding_year: 2002)
+# Company.create(name: 'Enron', founding_year: 1995)
 
-puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+# puts 'Creating devs...'
+# Dev.create(name: 'Rick')
+# Dev.create(name: 'Morty')
+# Dev.create(name: 'Mr. Meseeks')
+# Dev.create(name: 'Gazorpazop')
 
-puts "Creating freebies..."
+puts 'Creating freebies...'
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
-
-puts "Seeding done!"
+25.times do
+  Freebie.create(item_name: Faker::Food.unique.dish, value: rand(1..20), company_id: rand(1..4), dev_id: rand(1..4))
+end
+puts 'Seeding done!'
